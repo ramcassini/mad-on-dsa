7b23b529c30f41bc5489157d9a24cda40f4ddf2d
ram <theanfang.developers@gmail.com>
2022-07-23 08:59:07 +0530

    initial commit

---
+++ src/main/java/com.datastructures.linkedlist/ListNode.java
@@ -0,0 +1,14 @@
+package com.datastructures.linkedlist;
+
+public class ListNode {
+
+    int val;
+    char data;
+
+    ListNode next;
+    ListNode(int val){
+
+        this.val=val;
+        this.next=null;
+    }
+}
